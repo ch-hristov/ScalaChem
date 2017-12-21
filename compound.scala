a0afa666c542ea3dc285b57afd76d3f1ee2f0840
Pavlos <ppanteliad@gmail.com>
2017-12-21 19:32:31 +0100

    My first crap

---
+++ compound.scala
@@ -0,0 +1,9 @@
+package smiles
+/**
+ * Corresponds to a single record from the PubChem Compound database.
+ * The PubChem Compound database is constructed from the Substance database using a standardization and deduplication process. 
+ * Each Compound is uniquely identified by a CID.
+ */
+class Compound(var record : ) {
+    
+}
