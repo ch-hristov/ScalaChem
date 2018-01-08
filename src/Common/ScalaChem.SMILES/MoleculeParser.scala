65108753075349645a26d86f13448c258638cf3c
= <hristohristov54@gmail.com>
2018-01-08 22:24:34 +0100

    Add molecule parser wrapper over smilesParser

---
+++ src/Common/ScalaChem.SMILES/MoleculeParser.scala
@@ -0,0 +1,18 @@
+package Common.ScalaChem.SMILES
+
+import Common.ScalaChem.Infrastructure.BondType.BondType
+import Common.ScalaChem.Infrastructure.{IAtom, IMolecule}
+import Common.ScalaChem.MolGraph.Molecule
+
+class MoleculeParser extends SmilesParser {
+  var mol : IMolecule = null
+  def parseBond(atom1:IAtom, atom2 : IAtom, bondType: BondType): Boolean ={
+    mol.connect(atom1,atom2,bondType)
+    return true;
+  }
+
+  def parse(smiles : String): IMolecule={
+    mol = new Molecule()
+    return parseSmiles(smiles,parseBond,x => mol += (x))
+  }
+}
